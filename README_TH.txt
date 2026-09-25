4f4dc49b295ddb289a63377d368adcfeb50bc894
arnonjun <Arnonlove1886@gmail.com>
2026-09-25 16:55:35 +0700

    Add files via upload

---
+++ README_TH.txt
@@ -0,0 +1,6 @@
+Car Media v1.3
+- ชื่อใหม่ Car Media
+- ใช้รูปกุญแจซอลที่ส่งมาเป็น icon
+- Latest Frame ทั้ง Android และ PC: ไม่สะสมคิวภาพเก่า
+- แสดง Sent / Dropped ใน notification
+วิธีใช้: ปิด TRCC/InfoPanel > เปิด PC_Bridge > TEST PC CONNECTION > START SCREEN MIRROR > Entire screen > ทดสอบ YouTube
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,7 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
-android {
- namespace="com.tsk.trofeomirror"; compileSdk=35
- defaultConfig { applicationId="com.tsk.trofeomirror"; minSdk=30; targetSdk=35; versionCode=12; versionName="1.2" }
+android { namespace="com.tsk.carmedia"; compileSdk=35
+ defaultConfig { applicationId="com.tsk.carmedia"; minSdk=30; targetSdk=35; versionCode=13; versionName="1.3" }
  compileOptions { sourceCompatibility=JavaVersion.VERSION_17; targetCompatibility=JavaVersion.VERSION_17 }
- kotlinOptions { jvmTarget="17" }
-}
+ kotlinOptions { jvmTarget="17" } }
 kotlin { jvmToolchain(17) }
-dependencies {
- implementation("androidx.core:core-ktx:1.15.0")
- implementation("androidx.appcompat:appcompat:1.7.0")
- implementation("com.squareup.okhttp3:okhttp:4.12.0")
-}
+dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0"); implementation("com.squareup.okhttp3:okhttp:4.12.0") }
